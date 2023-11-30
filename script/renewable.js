ab8962781c7c0274a7400bd5b55eaece8bf37116
Andre <andrepontde@gmail.com>
2023-11-30 14:23:06 +0000

    Optimization for JS - Merged all files to one

--- script/renewable.js
+++
@@ -1,5 +0,0 @@
-function active(topChoice) {
-    var chosenTop = document.getElementById(topChoice);
-    var topText = chosenTop.innerHTML;
-    document.getElementById("activeTxt").innerHTML = topText
-}
