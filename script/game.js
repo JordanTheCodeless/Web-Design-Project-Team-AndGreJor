12bd57bd1f9274754ec47ecb606286ea59cb7e8c
Andre <andrepontde@gmail.com>
2023-12-06 12:23:55 -0600

    more js fixes

---
+++ script/game.js
@@ -0,0 +1,11 @@
+const TreesArray = [];
+
+function TreeArray() {
+    var tree = Math.floor(Math.random() * 7);
+    document.getElementById("GameTxt").innerHTML = "game" + tree + "tree";
+    document.getElementById("Game").innerHTML = "hello world";  
+}
+
+TreeArray();
+
+
--- script/renewable.js
+++ script/renewable.js
@@ -4,14 +4,3 @@ function active(topChoice) {
     document.getElementById("activeTxt").innerHTML = topText
 }
 
-const TreesArray = [];
-
-function TreeArray() {
-    var tree = Math.floor(Math.random() * 7);
-    document.getElementById("GameTxt").innerHTML = "game" + tree + "tree";
-    document.getElementById("Game").innerHTML = "hello world";  
-}
-
-TreeArray();
-
-
