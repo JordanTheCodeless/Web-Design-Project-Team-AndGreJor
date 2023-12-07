79d26899e84e9b4512521aee0e6af696ceaa18e7
Greg680 <PintalGreg@gmail.com>
2023-12-07 09:44:20 +0000

    game progress

--- script/script.js
+++ script/script.js
@@ -139,21 +139,7 @@ function subTotal(){
 
 
 //Greg
-function active(topChoice) {
-    var chosenTop = document.getElementById(topChoice);
-    var topText = chosenTop.innerHTML;
-    document.getElementById("activeTxt").innerHTML = topText
-}
 
-const TreesArray = [];
-
-function TreeArray() {
-    var tree = Math.floor(Math.random() * 7);
-    document.getElementById("GameTxt").innerHTML = "game" + tree + "tree";
-    document.getElementById("Game").innerHTML = "hello world";  
-}
-
-TreeArray();
 
 
 
