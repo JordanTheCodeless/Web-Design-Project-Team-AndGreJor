e45a77bbad6dc9f5fd03af8647d91ca1eaaca234
Greg680 <PintalGreg@gmail.com>
2023-12-07 15:37:05 +0000

    game progress

--- script/game.js
+++ script/game.js
@@ -1,17 +1,41 @@
-//   function active(topChoice) {
-//     var chosenTop = document.getElementById(topChoice);
-//     var topText = chosenTop.innerHTML;
-//     document.getElementById("").innerHTML = topText
+function RanTreeArray(arr) {
+  const RanTree = Math.floor(Math.random() * arr.length);
+  const tree = arr[RanTree];
+  arr.splice(RanTree, 1);
+  return tree;
+}
+
+const TreesArray = ['../images/Windmill.jpg', '../images/MultipleWindmills.jpg', '../images/SolarPanel.jpg', '../images/SolarFarm.jpg', '../images/Teacher1.jpg', '../images/Teacher2.jpg',];
+const TreeText = ['windmill pic 1', 'windmills pic', 'solar panel pic 1', 'solar panel pic 2', 'teacher pic 1', 'teacher pic 2',];
+
+const shownImg = document.getElementById("GameOutput")
+const nextBut = document.getElementById("nextBut")
+
+nextBut.addEventListener("click", nextImg)
+
+function nextImg() {
+    if (TreesArray.length > 1){
+        const ranInd = Math.floor(Math.random() * TreesArray.length);
+        const selectImg = TreesArray.splice(ranInd, 1)[0];
+        const selectTxt = TreeText.splice(ranInd, 1)[0];
+        shownImg.src = selectImg;
+
+        DisplayTxt = document.getElementById("Game").innerHTML = selectTxt
+        // const Dis = RanTreeArray(TreesArray)
+        // shownImg.src = Dis;
+}else{
+    const lastImg = TreesArray[0]
+    shownImg.src = lastImg
+    nextBut.disabled = true;
+}
+}
+nextImg();
+        
+
+// while (TreesArray.length > 1){
+//     const Dis = RanTreeArray(TreesArray);
+
 // }
-while (TreesArray.length > 0) {
-    function RanTreeArray(arr) {
-      const RanTree = Math.floor(Math.random() * arr.length);
-      const tree = arr[RanTree];
-      return tree;
-      TreesArray.pop(Rantree)
-    }
-  }
-  const TreesArray = ['../images/Windmill.jpg', '../images/MultipleWindmills.jpg', '../images/SolarPanel.jpg', '../images/SolarFarm.jpg', '../images/Teacher1.jpg', '../images/Teacher2.jpg',];
   
-  const Dis = RanTreeArray(TreesArray);
-  document.getElementById("GameOutput").src =  Dis;
+// const noImg = TreesArray(0);
+// document.getElementById("GameOutput").src =  Dis;
