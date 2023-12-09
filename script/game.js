4ac31acdf393ba246211e15fbfd1741748a45a3b
JordanTheCodeless <131815445+JordanTheCodeless@users.noreply.github.com>
2023-12-09 23:46:39 +0000

    removed all un-necessary whitespacing and blank lines

--- script/game.js
+++ script/game.js
@@ -4,28 +4,22 @@ function RanTreeArray(arr) {
   arr.splice(RanTree, 1);
   return tree;
 }
-
 const TreesArray = [{ imgpath: '../images/Windmill.jpg', Ans: 'windmill'},
 { imgpath: '../images/MultipleWindmills.jpg', Ans: 'windmills'},
 { imgpath: '../images/SolarPanel.jpg', Ans: 'solarpanel'},
 { imgpath: '../images/SolarFarm.jpg', Ans: 'solarfarm'},
 { imgpath: '../images/Teacher1.jpg', Ans: 'teacherpoint'},
 { imgpath:  '../images/Teacher2.jpg', Ans: 'teacherstudent'}];
-
 const shownImg = document.getElementById("GameOutput")
 const nextBut = document.getElementById("nextBut")
-
 nextBut.addEventListener("click", nextImg)
-
 function nextImg() {
     if (TreesArray.length > 1){
         const ranInd = Math.floor(Math.random() * TreesArray.length);
         const selectInd = TreesArray.splice(ranInd, 1)[0];
         const selectImg = selectInd.imgpath;
         const selectAns = selectInd.Ans;
-
         shownImg.src = selectImg;
-
         document.getElementById("Game").innerHTML = selectAns
         // const Dis = RanTreeArray(TreesArray)
         // shownImg.src = Dis;
@@ -37,12 +31,8 @@ function nextImg() {
 }
 }
 nextImg();
-        
-
 // while (TreesArray.length > 1){
 //     const Dis = RanTreeArray(TreesArray);
-
 // }
-  
 // const noImg = TreesArray(0);
 // document.getElementById("GameOutput").src =  Dis;
--- script/store.js
+++ script/store.js
@@ -1,12 +1,9 @@
 //André
-
 opt1(0);
 opt2(0);
 opt3(0);
 subTotal();
-
 //Called the functions first so the value of each object was set to 0, enabling the use of the subTotal function without the need of selecting all of the options
-
 //Function called in category 1 to determine the price of the selected object
 function opt1(optn1){
     switch (optn1){
@@ -22,13 +19,10 @@ function opt1(optn1){
         default:
         optn1value = 0;
             break;
-
     }
     document.getElementById("opt1tot").innerHTML = "Option 1 is: " + optn1value + "$";
 }
-
 //Function called in category 2 to determine the price of the selected object
-
 function opt2(optn2){
     switch (optn2){
         case 1:
@@ -43,13 +37,10 @@ function opt2(optn2){
         default:
         optn2value = 0;
             break;
-
     }
     document.getElementById("opt2tot").innerHTML = "Option 2 is: " + optn2value + "$";
 }
-
 //Function called in category 3 to determine the price of the selected object
-
 function opt3(optn3){
     switch (optn3){
         case 1:
@@ -64,65 +55,49 @@ function opt3(optn3){
         default:
         optn3value = 0;
             break;
-
     }
     document.getElementById("opt3tot").innerHTML = "Option 3 is: " + optn3value + "$";
 }
-
-
-
 //Code understood with the aid of W3School (Note reviewed on class but necesary for code functionality):
-
-//This function selects a div from the categories and deselects every other 
+//This function selects a div from the categories and deselects every other
 function selected(slc) {
     //Get the parent category div
     var category = slc.closest(".row");
-
     //Deselect all objects in the same category
     var objectsInCategory = category.querySelectorAll(".product");
     objectsInCategory.forEach(function (obj) {
         obj.classList.remove("selected");
     });
-
     //Select the clicked object
     slc.classList.add("selected");
 }
-
 //Code understood with the aid of W3School:
-
 function removeOne(){
     var element1 = document.getElementsByClassName("one")[0];
     var element2 = document.getElementsByClassName("one")[1];
     var element3 = document.getElementsByClassName("one")[2];
-
     element1.classList.remove("selected");
     element2.classList.remove("selected");
     element3.classList.remove("selected");
     //this targers the classes list and removes the "Selected" class from them when called
 }
-
 function removeTwo(){
     var element1 = document.getElementsByClassName("two")[0];
     var element2 = document.getElementsByClassName("two")[1];
     var element3 = document.getElementsByClassName("two")[2];
-
     element1.classList.remove("selected");
     element2.classList.remove("selected");
     element3.classList.remove("selected");
 }
-
 function removeThree(){
     var element1 = document.getElementsByClassName("three")[0];
     var element2 = document.getElementsByClassName("three")[1];
     var element3 = document.getElementsByClassName("three")[2];
-
     element1.classList.remove("selected");
     element2.classList.remove("selected");
     element3.classList.remove("selected");
 }
-
 //Function called in category 3 to determine the price of the selected object
-
 function subTotal(){
     var subtot = optn1value + optn2value + optn3value;
     document.getElementById("subtotal").innerHTML = "Subtotal is: " + subtot + "$";
@@ -134,5 +109,4 @@ function subTotal(){
     document.getElementById("totaInfo").innerHTML = "Total is: " + totmoney;
     document.getElementById("amnt").innerHTML = totmoney
 }
-
 //Function to modify and dinamically display the amount of money owed for each service
