0c0db47800b5c8200082b485b1d0988a24a93d43
Greg680 <PintalGreg@gmail.com>
2023-12-10 20:50:21 +0000

    merge

--- script/game.js
+++ script/game.js
@@ -1,70 +1,52 @@
-// function RanTreeArray(arr) {
-//   const RanTree = Math.floor(Math.random() * arr.length);
-//   const tree = arr[RanTree];
-//   arr.splice(RanTree, 1);
-//   return tree;
-// }
 
-var dataArray = [{ imgpath: '../images/Windmill.jpg', text: 'windmill'},
-{ imgpath: '../images/MultipleWindmills.jpg', text: 'windmills'},
-{ imgpath: '../images/SolarPanel.jpg', text: 'solarpanel'},
-{ imgpath: '../images/SolarFarm.jpg', text: 'solarfarm'},
-{ imgpath: '../images/Teacher1.jpg', text: 'teacherpoint'},
-{ imgpath:  '../images/Teacher2.jpg', text: 'teacherstudent'}];
+var imageArray = ["../images/gameSolar.webp", "../images/gameCompost.webp", "../images/gameElectric.webp", "../images/gameDeforestation.webp", "../images/gamePlastic.webp"]
+//Array for the images of the game
 
-var shownImg = document.getElementById("GameOutput")
-var nextBut = document.getElementById("nextBut")
-var scoreCounter = document.getElementById("scoreCounter")
-var gameAns = document.getElementById("GameAnswers")
-var score = 0
-var currentEntry
 
-nextBut.addEventListener("click", nextImg)
+var questionsArray = ["Solar energy is a renewable source of power.", "Composting is an effective way to reduce waste and benefit the environment.", "Electric vehicles produce zero emissions and are environmentally friendly.", "Deforestation contributes to climate change and loss of biodiversity.", "Single-use plastics are a sustainable option for reducing waste."];
+//Array for the questions of the game
 
-function nextImg() {
-  if (dataArray.length > 0) {
-    var randomIndex = Math.floor(Math.random() * dataArray.length);
-    currentEntry = dataArray.splice(randomIndex, 1)[0];
+var randomAnswers = [true, true, false, false, false];
+//Array for the answers of the game
 
-    shownImg.src = currentEntry.imgpath;
-    document.getElementById("randomGenerated").innerText = currentEntry.text;
- 
-    // Reset radio buttons
-    var radioButtons = document.getElementsByName("yesOrNo");
-    for (var i = 0; i < radioButtons.length; i++) {
-      radioButtons[i].checked = false;
+var currentQuestion = 0;
+var score = 0;
+
+//The input ...arrays lets you make the exact same output out of the 3 arrays, so when we call the math random function, it doesn't randomize the arrays in different order.
+function arrayRandomizer(...arrays){
+  var length = arrays[0].length;
+
+  for (let i = length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+
+    for (const array of arrays){
+      [array[i], array[j]] = [array[j], array[i]];
     }
- 
-    // Update score display
-    scoreCounter.innerText = "Score: " + sc
-}else{
-    nextBut.disabled = true;
   }
 }
 
-function checkAnswer() {
-  var radioButtons = document.getElementsByName("yesOrNo");
- 
-  for (var i = 0; i < radioButtons.length; i++) {
-    if (radioButtons[i].checked) {
-      var userAnswer = radioButtons[i].value.toLowerCase();
-      var correctAnswer = currentEntry.text.toLowerCase().replace(/\s/g, '');
+arrayRandomizer(imageArray, questionsArray, randomAnswers);
 
-      if (userAnswer === correctAnswer) {
-        score++;
-      } else {
-        score--;
-      }
-        // Update score display
-        scoreCounter.innerText = "Score: " + score;
-        break;
-        }
-      }
-    }
-    
-document.addEventListener("DOMContentLoaded", function () {
-nextImg();
-});
+showQuestion();
 
-// Add an event listener to the radio buttons
-gameAnswers.addEventListener("change", checkAnswer);
+function showQuestion() {
+  if (currentQuestion < questionsArray.length) {
+      document.getElementById("GameOutputImg").src = imageArray[currentQuestion];
+      document.getElementById("randomQuestion").innerHTML = questionsArray[currentQuestion];
+  } else {
+      // In case all questions have been answered
+      document.getElementById("randomQuestion").innerHTML = "Game Over! the final score was: " + score;
+  }
+}
+
+function checkAnswer(buttonClick){
+  var correctAnswer = randomAnswers[currentQuestion];
+
+  if (buttonClick == correctAnswer) {
+    score++;
+    document.getElementById("scoreCounter").textContent = "Score: " + score;
+  }
+  currentQuestion++;
+  showQuestion();
+
+}
